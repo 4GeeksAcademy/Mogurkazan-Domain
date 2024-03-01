2d2de1760d8b608c0637a134a85df3e2befa8304
Juan Antonio Cortes Elrio <152733103+Mogurkazan@users.noreply.github.com>
2024-03-01 20:11:41 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,17 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let ext = [".es", ".net", ".com"];
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let x = 0; x < ext.length; x++) {
+          console.log(pronoun[i] + adj[j] + noun[k] + ext[x]);
+        }
+      }
+    }
+  }
 };
